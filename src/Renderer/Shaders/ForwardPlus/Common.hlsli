455740627ac4a1b07a811128e7a7184afd55d2ff
Aqua <ollie@ollierobinson.co.uk>
2026-04-29 18:43:29 +0100

    fix shading bug on macos

--- src/Renderer/Shaders/ForwardPlus/Common.hlsli
+++ src/Renderer/Shaders/ForwardPlus/Common.hlsli
@@ -6,4 +6,6 @@
 #define TILE_SIZE 16
 #define MAX_LIGHTS_PER_TILE 512
 
+float4
+
 #endif
--- src/Renderer/Shaders/ForwardPlus/DepthPrepass.hlsl
+++ src/Renderer/Shaders/ForwardPlus/DepthPrepass.hlsl
@@ -17,7 +17,10 @@ cbuffer DrawData : register(b1, space1)
 
 float4 VSMain(const in Vertex input): SV_Position
 {
-    return mul(gScene.Camera.Projection, mul(gScene.Camera.View, mul(World, float4(input.Position, 1.0))));
+    float4 value = mul(gScene.Camera.Projection, mul(gScene.Camera.View, mul(World, float4(input.Position, 1.0))));
+    value.z += 1e-5; // For some reason on macos the color pass and depth pass have precision issues and so you get
+                     // graphical glitches. Adding an epsilon value seems to fix it. I don't like it but here we are
+    return value;
 }
 
 float4 PSMain(): SV_Target0
